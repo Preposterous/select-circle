b83a7a6010d5e6599fc7b14dc29d15d918d744ad
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2018-02-23 21:36:19 -0800

    init

---
+++ SelectCircle.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+export const SelectCircle = ({
+  selected,
+  selectedColor,
+  deselectedColor,
+  style
+}) => {
+  const outerCircleStyles = [
+    SelectCircleStyles.outerCircle,
+    {
+      backgroundColor: deselectedColor
+    },
+    style
+  ];
+
+  const innerCircleStyles = [
+    SelectCircleStyles.innerCircle,
+    {
+      borderColor: deselectedColor
+    }
+  ];
+
+  if (selected) {
+    outerCircleStyles.push({
+      backgroundColor: selectedColor,
+      borderColor: selectedColor
+    });
+    innerCircleStyles.push({
+      backgroundColor: "white"
+    });
+  }
+
+  return (
+    <View style={outerCircleStyles}>
+      <View style={innerCircleStyles} />
+    </View>
+  );
+};
+
+SelectCircle.defaultProps = {
+  selectedColor: "rgb(0, 175, 115)",
+  deselectedColor: "rgba(129, 161, 186, 0.5)"
+};
+
+const SelectCircleStyles = StyleSheet.create({
+  outerCircle: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    justifyContent: "center",
+    alignItems: "center",
+    borderStyle: "solid",
+    borderWidth: 1,
+    backgroundColor: "#fff"
+  },
+  innerCircle: {
+    width: 8,
+    height: 8,
+    borderRadius: 4
+  }
+});
